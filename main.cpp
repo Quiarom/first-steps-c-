266f8b4765f07c78e543ca203b9c9bd90b42f8a1
Daniel Quiaro <75100376+Quiarom@users.noreply.github.com>
2021-07-10 15:10:16 +0000

    Const and List

--- main.cpp
+++ main.cpp
@@ -2,13 +2,20 @@
 
 using namespace std;
 using std::cout;
+using std::endl;
 
 int main() {
 
-	int edad = 0;
-	edad = 8;
+	int age = 0;
+	age = 18;
 
-	cout << edad;
+	const char letter = 'a';
 
+	int ages_people[] = { 18, 24, 16 };
+	ages_people[1] = 26;
+
+	cout << age << endl;
+	cout << letter << endl;
+	cout << ages_people[1] << endl;
 
 }
